dd3141914b97b54a39576fb94e2ef51c1285e682
unknown <I318422@INLN50888463A.apj.global.corp.sap>
2015-05-01 22:45:47 +0530

    -a

--- camel-apache-platform/src/main/ws-payments-apis/org/camelcookbook/ws/payment_service/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.camelcookbook.org/payment-service/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.camelcookbook.ws.payment_service.types;
